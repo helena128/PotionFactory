4b96ee142d4ac440ee328abb112ff274046d9986
Yaroslav Rogov <rogovyaroslav@gmail.com>
2020-10-27 13:42:33 +0300

    fix: Fix tags for ingredients

--- app/graphql/ingredient/Queries.scala
+++ app/graphql/ingredient/Queries.scala
@@ -8,13 +8,14 @@ import sangria.schema.{Field, ListType, ObjectType, fields}
 import security.AppContext
 
 object Queries extends graphql.Queries {
+  private val tags = List(FairyTag, WorkerTag)
   val queries = ObjectType("IngredientQuery", "Ingredient Queries",
     fields[AppContext, Unit](
       Field("ingredient", IngredientType,
         arguments = IdInt :: Nil,
         resolve = c => ingredientFetcher.defer(c.arg(IdInt)),
-        tags = WorkerTag :: Nil),
+        tags = tags),
       Field("allIngredients", ListType(IngredientType),
         resolve = c => c.ctx.dao.getAllIngredients,
-        tags = WorkerTag :: Nil)))
+        tags = tags)))
 }
