e9299528ef814567eb1600af0269cb7fda4c1f20
Yaroslav Rogov <rogovyaroslav@gmail.com>
2020-10-28 23:18:33 +0300

    feat: Add allRequests

--- app/graphql/request/Queries.scala
+++ app/graphql/request/Queries.scala
@@ -7,12 +7,15 @@ import sangria.schema._
 import security.AppContext
 
 object Queries extends graphql.Queries {
+  val tags = List(ManagerTag, WarehouseTag)
   val queries = ObjectType("IngredientRequestQuery", "Ingredient Request Queries",
     fields[AppContext, Unit](
       Field("request", IngredientRequestType,
         arguments = List(IdInt),
         resolve = c => c.ctx.dao.getIngredientRequest(c.arg(IdInt)),
-        tags = List(ManagerTag, WarehouseTag)
-      )
+        tags = tags),
+      Field("allRequests", ListType(IngredientRequestType),
+        resolve = c => c.ctx.dao.getAllIngredientRequests(),
+        tags = tags)
     ))
 }
