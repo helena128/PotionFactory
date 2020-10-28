697d14cfd9cdb05c45fcc3ea37e314d26ac871d3
Yaroslav Rogov <rogovyaroslav@gmail.com>
2020-10-28 23:18:42 +0300

    feat: Add allReports

--- app/graphql/transfer/Queries.scala
+++ app/graphql/transfer/Queries.scala
@@ -7,11 +7,15 @@ import sangria.schema._
 import security.AppContext
 
 object Queries extends graphql.Queries {
+  val tags = List(ManagerTag, WorkshopTag)
   val queries = ObjectType("ProductTransferQuery", "Product Transfer Queries",
     fields[AppContext, Unit](
       Field("report", ProductTransferType,
         arguments = List(IdInt),
         resolve = c => c.ctx.dao.getProductTransfer(c.arg(IdInt)),
-        tags = List(ManagerTag)
-      )))
+        tags = tags),
+      Field("allReports", ListType(ProductTransferType),
+        resolve = c => c.ctx.dao.getAllProductTransfers(),
+        tags = tags)
+    ))
 }
